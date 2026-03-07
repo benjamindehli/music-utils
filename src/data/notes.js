d3c623bb8cf7c8e018f725b6aa4a9079845123e8
Benjamin Dehli <superelg@gmail.com>
2026-03-07 18:23:55 +0100

    Refactor note and chord data to use classes
    Updates the static data definitions in notes.js and chords.js to use the recently added Note and Chord class instances instead of plain objects.

--- src/data/notes.js
+++ src/data/notes.js
@@ -1,16 +1,19 @@
+// Classes
+import Note from "../classes/Note";
+
 const notes = [
-    { number: 0, name: "C" },
-    { number: 1, name: "C#" },
-    { number: 2, name: "D" },
-    { number: 3, name: "D#" },
-    { number: 4, name: "E" },
-    { number: 5, name: "F" },
-    { number: 6, name: "F#" },
-    { number: 7, name: "G" },
-    { number: 8, name: "G#" },
-    { number: 9, name: "A" },
-    { number: 10, name: "A#" },
-    { number: 11, name: "B" }
+    new Note({ number: 0, name: "C" }),
+    new Note({ number: 1, name: "C#" }),
+    new Note({ number: 2, name: "D" }),
+    new Note({ number: 3, name: "D#" }),
+    new Note({ number: 4, name: "E" }),
+    new Note({ number: 5, name: "F" }),
+    new Note({ number: 6, name: "F#" }),
+    new Note({ number: 7, name: "G" }),
+    new Note({ number: 8, name: "G#" }),
+    new Note({ number: 9, name: "A" }),
+    new Note({ number: 10, name: "A#" }),
+    new Note({ number: 11, name: "B" })
 ];
 
 export default notes;
