1546f12a4c0f65039bd6ec19ae759d4a3c58966a
Benjamin Dehli <superelg@gmail.com>
2026-03-14 15:28:49 +0100

    Expand package exports to include core classes
    Update the main entry point to export the Chord, Midi, NoteSelection, and Scale classes, making the library's core logic accessible to consumers.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+// Classes
+import Chord from "./classes/Chord.js";
+import Midi from "./classes/Midi.js";
+import NoteSelection from "./classes/NoteSelection.js";
+import Scale from "./classes/Scale.js";
+
+// Data
+import chords from "./data/chords.js";
+import notes from "./data/notes.js";
+import scales from "./data/scales.js";
+
+export { chords, scales, notes, Midi, Chord, Scale, NoteSelection };
+
 export { getChordsFromSelectedNotes } from "./helpers/matchHelpers.js";
 export { getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./helpers/noteHelpers.js";
-export { default as chords } from "./data/chords.js";
-export { default as scales } from "./data/scales.js";
-export { default as notes } from "./data/notes.js";
