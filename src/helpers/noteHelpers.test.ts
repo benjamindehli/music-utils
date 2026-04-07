909a478fa4776784a3cb29eb8070b8cdb019e9df
Benjamin Dehli <superelg@gmail.com>
2026-04-07 21:37:56 +0200

    Update unit tests

--- src/helpers/noteHelpers.test.ts
+++ src/helpers/noteHelpers.test.ts
@@ -1,4 +1,4 @@
-import { getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./noteHelpers";
+import { getAbsoluteNoteNumber, getNoteFromNoteNumber, getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./noteHelpers";
 
 describe("noteHelpers", () => {
     describe("getRelativeNoteNumber", () => {
@@ -12,12 +12,33 @@ describe("noteHelpers", () => {
         });
     });
 
+    describe("getAbsoluteNoteNumber", () => {
+        it("returns correct absolute note number within octave", () => {
+            expect(getAbsoluteNoteNumber(0, 60)).toBe(0); // C
+            expect(getAbsoluteNoteNumber(1, 60)).toBe(1); // C#
+            expect(getAbsoluteNoteNumber(11, 60)).toBe(11); // B
+            expect(getAbsoluteNoteNumber(12, 60)).toBe(0); // C (wraps)
+            expect(getAbsoluteNoteNumber(13, 60)).toBe(1); // C# (wraps)
+        });
+    });
+
     describe("normalizeHalfStep", () => {
         it("returns the note number modulo 12", () => {
             expect(normalizeHalfStep(60)).toBe(0);
             expect(normalizeHalfStep(61)).toBe(1);
             expect(normalizeHalfStep(71)).toBe(11);
             expect(normalizeHalfStep(72)).toBe(0);
+        });
+    });
+
+    describe("getNoteFromNoteNumber", () => {
+        it("returns the correct Note object for a given MIDI note number", () => {
+            const noteC = getNoteFromNoteNumber(60);
+            expect(noteC).toBeDefined();
+            expect(noteC?.name).toBe("C");
+            const noteG = getNoteFromNoteNumber(67);
+            expect(noteG).toBeDefined();
+            expect(noteG?.name).toBe("G");
         });
     });
 
