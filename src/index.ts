eef4b24cdd70f19226c6e1d5b7e8ee955187b302
Benjamin Dehli <superelg@gmail.com>
2026-04-17 21:36:32 +0200

    Export additional classes and helper functions
    Exposes the ChordMatch, Note, and ScaleMatch classes, along with helpers for retrieving scales and notes, making them available in the package's public API.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 // Classes
 import Chord from "./classes/Chord";
+import ChordMatch from "./classes/ChordMatch";
 import ChordType from "./classes/ChordType";
 import Interval from "./classes/Interval";
 import Midi from "./classes/Midi";
+import Note from "./classes/Note";
 import NoteSelection from "./classes/NoteSelection";
 import Scale from "./classes/Scale";
+import ScaleMatch from "./classes/ScaleMatch";
 import ScaleType from "./classes/ScaleType";
 
 // Data
@@ -13,8 +16,8 @@ import intervals from "./data/intervals";
 import notes from "./data/notes";
 import scaleTypes from "./data/scaleTypes";
 
-export { Chord, ChordType, Interval, Midi, NoteSelection, Scale, ScaleType, chordTypes, intervals, notes, scaleTypes };
+export { Chord, ChordMatch, ChordType, Interval, Midi, Note, NoteSelection, Scale, ScaleMatch, ScaleType, chordTypes, intervals, notes, scaleTypes };
 
 // Helpers
-export { getChordsFromSelectedNotes } from "./helpers/matchHelpers";
-export { getAbsoluteNoteNumber, getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./helpers/noteHelpers";
+export { getChordsFromSelectedNotes, getScalesFromSelectedNotes } from "./helpers/matchHelpers";
+export { getAbsoluteNoteNumber, getNoteFromNoteNumber, getRelativeNoteNumber, normalizeHalfStep, normalizeHalfSteps } from "./helpers/noteHelpers";
